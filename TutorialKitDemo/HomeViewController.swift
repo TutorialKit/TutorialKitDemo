fa505bb6f69cdf63d520ef2802bd16a490a69245
Lee Cooper <leecooper@Lees-MacBook-Pro.local>
2016-07-22 22:26:35 -0700

    Added 2 buttons to home screen
    Added 2 buttons to home screen.
    Also added new Home and Settings View Controller files. (delete
    first/second view controller files in future commit).

---
+++ TutorialKitDemo/HomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeViewController.swift
+//  TutorialKitDemo
+//
+//  Created by Lee Cooper on 7/22/16.
+//  Copyright © 2016 Aqueous Software. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var iWinButton: UIButton!
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        iWinButton.enabled = true
+    }
+    
+    @IBAction func iWinButtonPressed(sender: AnyObject) {
+        print("I win button pressed")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        iWinButton.enabled = false
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ TutorialKitDemo/SettingsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsViewController.swift
+//  TutorialKitDemo
+//
+//  Created by Lee Cooper on 7/22/16.
+//  Copyright © 2016 Aqueous Software. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
