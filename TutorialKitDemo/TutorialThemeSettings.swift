b140e15c94dd634508f6726618ffd80424593ba1
Lee Cooper <leecooper@Lees-MacBook-Pro.local>
2016-07-24 13:13:29 -0700

    Increase buttons in Tap to 80x80

--- TutorialKitDemo/TutorialThemeSettings.swift
+++ TutorialKitDemo/TutorialThemeSettings.swift
@@ -14,9 +14,34 @@ class TutorialThemeSettings {
     var themeBackgroundColor = UIColor.blackColor()
     
     init() {
+
         let theme = TKBannerTutorialView.defaultTheme()
         themeForegroundColor = theme.foregroundColor
         themeBackgroundColor = theme.backgroundColor
+
+        // check to see if themes have been previously saved; if not, use defaults
+        //        let prefs = NSUserDefaults.standardUserDefaults()
+        //
+        //        prefs.setValue("Berlin", forKey: "userCity")
+        //        //This code saves the value "Berlin" to a key named "userCity".
+        
+        //        // reading data
+        //        if let city = prefs.stringForKey("userCity"){
+        //            println("The user has a city defined: " + city)
+        //        }else{
+        //            //Nothing stored in NSUserDefaults yet. Set a value.
+        //            prefs.setValue("Berlin", forKey: "userCity")
+        //        }
+        
+//        
+//        let prefs = NSUserDefaults.standardUserDefaults()
+//        if let color = prefs.stringForKey("foreground") {
+//            themeForegroundColor = color
+//        } else {
+//            //Nothing stored in NSUserDefaults yet. Set a value.
+//            themeForegroundColor = theme.foregroundColor
+//        }
+        
     }
     
 }
