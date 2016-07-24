5548c5c9c2ce802ab60557d7ce299479f7755d49
Lee Cooper <leecooper@Lees-MacBook-Pro.local>
2016-07-23 22:57:14 -0700

    Added Tap and Balloon view controller files
    Added Tap and Balloon view controller files

---
+++ TutorialKitDemo/BalloonViewController.swift
@@ -0,0 +1,27 @@
+//
+//  BalloonViewController.swift
+//  TutorialKitDemo
+//
+//  Created by Lee Cooper on 7/23/16.
+//  Copyright © 2016 Aqueous Software. All rights reserved.
+//
+
+import Foundation
+
+class BalloonViewController: UIViewController {
+    
+    @IBOutlet weak var swiperLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillAppear(animated: Bool)
+    {
+        super.viewWillAppear(animated)
+    }
+}
---
+++ TutorialKitDemo/TapViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TapViewController.swift
+//  TutorialKitDemo
+//
+//  Created by Lee Cooper on 7/23/16.
+//  Copyright © 2016 Aqueous Software. All rights reserved.
+//
+
+import Foundation
+
+class TapViewController: UIViewController {
+    
+    @IBOutlet weak var swiperLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillAppear(animated: Bool)
+    {
+        super.viewWillAppear(animated)
+    }
+}
