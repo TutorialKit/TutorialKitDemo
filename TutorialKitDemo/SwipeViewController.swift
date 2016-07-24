285619ed499e13b6a3bcc14add02db05b7dc1edc
Lee Cooper <leecooper@Lees-MacBook-Pro.local>
2016-07-23 22:45:58 -0700

    Swipe enabled
    Swipe enabled

---
+++ TutorialKitDemo/SwipeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SwipeViewController.swift
+//  TutorialKitDemo
+//
+//  Created by Lee Cooper on 7/23/16.
+//  Copyright © 2016 Aqueous Software. All rights reserved.
+//
+
+import Foundation
+
+class SwipeViewController: UIViewController {
+    
+    @IBOutlet weak var swiperLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: "respond:")
+        swipeRight.direction = .Right
+        view.addGestureRecognizer(swipeRight)
+    }
+    
+    func respond(gesture: UIGestureRecognizer) {
+        swiperLabel.text = "Swiped Right"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillAppear(animated: Bool)
+    {
+        super.viewWillAppear(animated)
+    }
+}
